bd8b16c080215fc09601ca165c31920f1bb0a939
Scott <scottdjevans@gmail.com>
2025-06-06 05:16:26 +0100

    add initial ApproveSection with mock data

--- src/app/document-search/components/Dashboard.js
+++ src/app/document-search/components/Dashboard.js
@@ -1,12 +1,13 @@
 "use client"
 
+import { MOCK_REGEX_VALUES } from "../mock-data"
 import MainDocumentArea from "./MainDocumentArea"
 import Sidebar from "./Sidebar"
 
-const Dashboard = () => {
+const Dashboard = ({ regexData = MOCK_REGEX_VALUES }) => {
   return (
     <div>
-      <Sidebar/>
+      <Sidebar regexData={regexData}/>
       <MainDocumentArea />
     </div>
   )
