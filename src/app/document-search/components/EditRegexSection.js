c4e46238678e02760330ebb04af7d36b4075dcd1
ScottDJEvans <scottdjevans@gmail.com>
2025-06-06 07:05:38 +0100

    Feature/add initial edit mode (#6)
    * add initial EditRegexSection, add Save UI for EditRegexSection
    
    * add delete click functionality, add layout styling
    
    * add cancel button for editing Regex, refactor repeated code to use single call for multiple actions required upon close
    
    * add initial Add button for EditRegexSection

---
+++ src/app/document-search/components/EditRegexSection.js
@@ -0,0 +1,84 @@
+import { Button } from "@/components/ui/button"
+import PropTypes from "prop-types"
+import { useState } from "react"
+
+const EditRegexSection = ({ options, handleEdit, handleDelete }) => {
+  const [isEditing, setIsEditing] = useState(null)
+  const [newRegexValue, setNewRegexValue] = useState("")
+
+  const handleCloseEdit = () => {
+    setIsEditing(null)
+    setNewRegexValue("")
+  }
+
+  const handleSaveClick = () => {
+    handleEdit(newRegexValue, isEditing)
+    handleCloseEdit()
+  }
+
+  const handleDeleteClick = () => {
+    handleDelete(isEditing)
+    handleCloseEdit()
+  }
+
+  const renderEditMode = (value) => {
+    return (
+      <div className="items-center my-2 max-w-[80%]">
+        <p>Editing {value}</p>
+        <input
+          defaultValue={value}
+          onChange={(e) => setNewRegexValue(e.target.value)}
+        />
+        <Button onClick={() => handleSaveClick()}>Save</Button>
+        <Button onClick={() => handleDeleteClick()}>Delete</Button>
+      </div>
+    )
+  }
+
+  const renderCancelButton = () => {
+    if (!isEditing) return null
+    return <Button onClick={() => handleCloseEdit()}>Cancel</Button>
+  }
+
+  const renderAddButton = () => {
+    if (isEditing) return null
+    return (
+      <>
+        <Button onClick={() => setIsEditing("new")}>Add</Button>
+      </>
+    )
+  }
+
+  const renderData = () => {
+    return options.map((data) => {
+      return (
+        <div className="flex flex-col w-full items-center">
+          <Button
+            onClick={() => setIsEditing(data.value)}
+            className="bg-tertiary max-w-[80%]"
+          >
+            {data.value}
+          </Button>
+          {isEditing === data.value && renderEditMode(data.value)}
+        </div>
+      )
+    })
+  }
+  return (
+    <div className="flex flex-col items-center m-4 max-w-[80%]">
+      <p>Expressions</p>
+      {renderData()}
+      {renderAddButton()}
+      {isEditing && renderCancelButton()}
+    </div>
+  )
+}
+
+export default EditRegexSection
+
+EditRegexSection.propTypes = {
+  options: PropTypes.array,
+  handleAdd: PropTypes.func,
+  handleEdit: PropTypes.func,
+  handleDelete: PropTypes.func,
+}
