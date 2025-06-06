254fd36dd34dc66b931e8142d9cd60d792d6670a
Scott <scottdjevans@gmail.com>
2025-06-06 09:57:43 +0100

    fix input field render logic to show when Add CTA clicked, add logic for ADD action

--- src/app/document-search/components/EditRegexSection.js
+++ src/app/document-search/components/EditRegexSection.js
@@ -3,10 +3,7 @@ import PropTypes from "prop-types"
 import { useState } from "react"
 import { REGEX_ACTIONS } from "./constants"
 
-const EditRegexSection = ({
-  options,
-  handleRegexChange
-}) => {
+const EditRegexSection = ({ options, handleRegexChange }) => {
   const [isEditing, setIsEditing] = useState(null)
   const [newRegexValue, setNewRegexValue] = useState("")
 
@@ -16,7 +13,8 @@ const EditRegexSection = ({
   }
 
   const handleSaveClick = () => {
-    handleRegexChange(REGEX_ACTIONS.EDIT, isEditing, newRegexValue)
+    const action = isEditing === "new" ? REGEX_ACTIONS.ADD : REGEX_ACTIONS.EDIT
+    handleRegexChange(action, isEditing, newRegexValue)
     handleCloseEdit()
   }
 
@@ -30,14 +28,16 @@ const EditRegexSection = ({
       <div className="items-center my-2 max-w-[80%]">
         <p>Editing {value}</p>
         <input
-          className="max-w-[120px] my-2"
+          className="max-w-[120px] my-2 bg-white rounded"
           defaultValue={value}
           onChange={(e) => setNewRegexValue(e.target.value)}
         />
         <Button className="my-2" onClick={() => handleSaveClick()}>
           Save
         </Button>
-        <Button className="bg-destructive" onClick={() => handleDeleteClick()}>Delete</Button>
+        <Button className="bg-destructive" onClick={() => handleDeleteClick()}>
+          Delete
+        </Button>
       </div>
     )
   }
@@ -49,23 +49,21 @@ const EditRegexSection = ({
 
   const renderAddButton = () => {
     if (isEditing) return null
-    return (
-      <>
-        <Button onClick={() => setIsEditing("new")}>Add</Button>
-      </>
-    )
+    return <Button onClick={() => setIsEditing("new")}>Add</Button>
   }
 
   const renderData = () => {
     return options.map((data) => {
       return (
         <div className="flex flex-col w-full items-center">
-        { !isEditing && <Button
-            onClick={() => setIsEditing(data.value)}
-            className="bg-tertiary max-w-[80%]"
+          {!isEditing && (
+            <Button
+              onClick={() => setIsEditing(data.value)}
+              className="bg-tertiary max-w-[80%]"
             >
-            {data.value}
-            </Button>}
+              {data.value}
+            </Button>
+          )}
           {isEditing === data.value && renderEditMode(data.value)}
         </div>
       )
@@ -74,6 +72,7 @@ const EditRegexSection = ({
   return (
     <div className="flex flex-col items-center text-center m-4 max-w-[150px]">
       <p>Editing expressions</p>
+      {isEditing === "new" && renderEditMode("")}
       {renderData()}
       {renderAddButton()}
       {isEditing && renderCancelButton()}
@@ -85,5 +84,5 @@ export default EditRegexSection
 
 EditRegexSection.propTypes = {
   options: PropTypes.array,
-  handleRegexChange: PropTypes.func
+  handleRegexChange: PropTypes.func,
 }
