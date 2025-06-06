88ed5e38bdfe42077c66bbb32899ef4fed136cd5
Scott <scottdjevans@gmail.com>
2025-06-06 04:40:08 +0100

    add initial barebones DocumentSearchPage, add initial Dashboard components

---
+++ src/app/document-search/components/Sidebar.js
@@ -0,0 +1,11 @@
+"use client"
+const Sidebar = () => {
+  return (
+    <aside>
+      <div>Approve Placeholder</div>
+      <div>Edit Placeholder</div>
+    </aside>
+  )
+}
+
+export default Sidebar
---
+++ src/app/document-search/page.js
@@ -0,0 +1,10 @@
+import Dashboard from "./components/Dashboard"
+
+export default function DocumentSearchPage() {
+  return (
+    <>
+      <div>Hello World!</div>
+      <Dashboard />
+    </>
+  )
+}
