c2cb6752188833ed7b1da60624db09d1147178dd
Scott <scottdjevans@gmail.com>
2025-06-06 05:58:30 +0100

    fix initial styling for Sidebar for debugging/initial layout

--- src/app/document-search/components/Sidebar.js
+++ src/app/document-search/components/Sidebar.js
@@ -9,20 +9,27 @@ const Sidebar = ({ regexData }) => {
   const [shouldShowApprovalSection, setShouldShowApprovalSection] =
     useState(false)
   return (
-    <aside>
-      <Button
-        onClick={() => setShouldShowApprovalSection(!shouldShowApprovalSection)}
-      >
-        Switch Mode
-      </Button>
-      {shouldShowApprovalSection ? <ApproveSection options={regexData} /> : <div>Edit Placeholder</div>}
+    <aside className="w-64 mr-6 rounded min-h-full bg-gray-300 block top-0 left-0 ">
+      <div className="mt-2 flex place-self-center">
+        <Button
+          onClick={() =>
+            setShouldShowApprovalSection(!shouldShowApprovalSection)
+          }
+        >
+          Switch Mode
+        </Button>
+      </div>
+      {shouldShowApprovalSection ? (
+        <ApproveSection options={regexData} />
+      ) : (
+        <div>Edit Placeholder</div>
+      )}
     </aside>
   )
 }
 
 export default Sidebar
 
-
 Sidebar.propTypes = {
-    regexData: PropTypes.array
+  regexData: PropTypes.array,
 }
