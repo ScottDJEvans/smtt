c8be4a838e61783090c60bda81bd78c84da29c34
Scott <scottdjevans@gmail.com>
2025-06-06 07:36:02 +0100

    fix lorem ipsum re-render by SSR via initial page call

--- src/app/document-search/page.js
+++ src/app/document-search/page.js
@@ -1,10 +1,12 @@
 import Dashboard from "./components/Dashboard"
+import { getInitialDocumentText } from "./components/utils"
 
 export default function DocumentSearchPage() {
+    const initialText = getInitialDocumentText()
   return (
     <>
       <div>Hello World!</div>
-      <Dashboard />
+      <Dashboard initialText={initialText} />
     </>
   )
 }
