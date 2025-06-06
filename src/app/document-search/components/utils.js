b2ba50b6f8fb1408319bb39479a2beca1b173003
Scott <scottdjevans@gmail.com>
2025-06-06 07:36:55 +0100

    add helper function to search regex in text, fix mock data to reflect helper addition

--- src/app/document-search/components/utils.js
+++ src/app/document-search/components/utils.js
@@ -3,3 +3,13 @@ import { loremIpsum } from "lorem-ipsum"
 export const getInitialDocumentText = () => {
   return loremIpsum({ count: 20 })
 }
+
+export function getRegexTextSearchResults(input, text) {
+    try {
+      const regex = new RegExp(input, "g")
+      const results = text.match(regex)
+      return results
+    } catch (e) {
+      console.log("Error: ", e)
+    }
+}
